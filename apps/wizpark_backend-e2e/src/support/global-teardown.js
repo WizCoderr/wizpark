dfd91f75b8bd3f43717af76025426f148d02ea7a
wizcoderr Sayam <ssayam200@gmail.com>
2024-09-07 16:26:53 +0530

    backend_done

--- apps/wizpark_backend-e2e/src/support/global-teardown.js
+++ apps/wizpark_backend-e2e/src/support/global-teardown.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-
 module.exports = async function () {
   // Put clean up logic here (e.g. stopping services, docker-compose, etc.).
   // Hint: `globalThis` is shared between setup and teardown.
--- apps/wizpark_backend-e2e/src/support/test-setup.js
+++ apps/wizpark_backend-e2e/src/support/test-setup.js
@@ -1,7 +1,5 @@
 /* eslint-disable */
-
 import axios from 'axios';
-
 module.exports = async function () {
   // Configure axios for tests to use.
   const host = process.env.HOST ?? 'localhost';
